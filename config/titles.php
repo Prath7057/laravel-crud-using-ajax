1827e5e9ed61d45d4e88d12ee6ce236097a01f50
Prath7057 <iamprathpatil@gmail.com>
2025-02-19 01:04:21 +0530

    Delete all files

--- config/titles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Str;
-
-return [
-    'form' => 'Laravel | Ajax | Form',
-    'form1' => 'Laravel | Ajax | Form1',
-    'dashboard' => 'Laravel | Ajax | Dashboard',
-];
